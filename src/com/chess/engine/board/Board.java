5fa052dd83614b09a52422eb96ffabf31649dd59
Fatih Durukan <fatihdurukan42@gmail.com>
2019-04-23 21:21:34 -0500

    EnPassant Move

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -174,6 +174,7 @@ public class Board {
 
         Map<Integer, Piece> boardConfig;
         Alliance nextMoveMaker;
+        Pawn enPassantPawn;
 
         public Builder (){
             this.boardConfig = new HashMap<>();
@@ -192,6 +193,10 @@ public class Board {
 
         public Board build(){
             return new Board(this);
+        }
+
+        public void setEnPassantPawn(Pawn enPassantPawn) {
+                this.enPassantPawn = enPassantPawn;
         }
     }
 }
--- src/com/chess/engine/board/Move.java
+++ src/com/chess/engine/board/Move.java
@@ -1,5 +1,6 @@
 package com.chess.engine.board;
 
+import com.chess.engine.pieces.Pawn;
 import com.chess.engine.pieces.Piece;
 
 import static com.chess.engine.board.Board.*;
@@ -30,6 +31,43 @@ public abstract class Move {
 
     public int getCurrentCoordinate(){
         return this.movedPiece.getPiecePosition();
+    }
+
+    public boolean isAttack(){
+        return false;
+    }
+
+    public boolean isCastlingMove(){
+        return false;
+    }
+
+    public Piece getAttackedPiece(){
+        return null;
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + this.movedPiece.getPiecePosition();
+        result = prime * result + this.movedPiece.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object other){
+        if(this == other){
+            return true;
+        }
+
+        if(!(other instanceof Move)){
+            return false;
+        }
+
+        final Move otherMove = (Move) other;
+        return getDestinationCoordinate() == otherMove.getDestinationCoordinate() &&
+                getMovedPiece().equals(otherMove.getMovedPiece());
     }
 
     public Board execute(){
@@ -88,6 +126,34 @@ public abstract class Move {
         public Board execute(){
             return null;
         }
+
+        @Override
+        public boolean isAttack(){
+            return true;
+        }
+
+        @Override
+        public Piece getAttackedPiece(){
+            return this.attackedPiece;
+        }
+
+        @Override
+        public int hashCode(){
+            return this.attackedPiece.hashCode() + super.hashCode();
+        }
+
+        @Override
+        public boolean equals(final Object other){
+            if(this == other){
+                return true;
+            }
+            if(!(other instanceof AttackMove)){
+                return false;
+            }
+            final AttackMove otherAttackMove = (AttackMove) other;
+            return super.equals(otherAttackMove) && getAttackedPiece().equals(otherAttackMove.getAttackedPiece());
+        }
+
     }
 
     //SUB-CLASSES
@@ -132,6 +198,27 @@ public abstract class Move {
                         final Piece piece,
                         final int destinationCoordinate) {
             super(board, piece, destinationCoordinate);
+        }
+
+        @Override
+        public Board execute(){
+            final Builder builder = new Builder();
+
+            for(final Piece piece : this.board.getCurrentPlayer().getActivePieces()){
+                if(!this.movedPiece.equals(piece)){
+                    builder.setPiece(piece);
+                }
+            }
+            for(final Piece piece : this.board.getCurrentPlayer().getOpponent().getActivePieces()){
+                builder.setPiece(piece);
+            }
+
+            final Pawn movedPawn = (Pawn) this.movedPiece.movePiece(this);
+            builder.setPiece(movedPawn);
+            builder.setEnPassantPawn(movedPawn);
+            builder.setMoveMaker(this.board.getCurrentPlayer().getOpponent().getAlliance());
+            return builder.build();
+
         }
 
     }
