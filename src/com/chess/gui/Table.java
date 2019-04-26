347cc3829c946edf1fd20456a98f4ec5e16c1d25
Fatih Durukan <fatihdurukan42@gmail.com>
2019-04-25 20:28:03 -0500

    Implemented TakenPiecesPanel

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -174,7 +174,39 @@ public class Table {
         }
     }
 
+    public static class MoveLog{
 
+        private final List<Move> moves;
+
+        MoveLog(){
+            this.moves = new ArrayList<>();
+        }
+
+        public List<Move> getMoves(){
+            return this.moves;
+        }
+
+        public void addMove(final Move move){
+            this.moves.add(move);
+        }
+
+        public int size(){
+            return this.moves.size();
+        }
+
+        public void clear(){
+            this.moves.clear();
+        }
+
+        public boolean removeMove(final Move move){
+            return this.moves.remove(move);
+        }
+
+        public Move removeMove(int index){
+            return this.moves.remove(index);
+        }
+
+    }
 
 
     private class TilePanel extends JPanel{
